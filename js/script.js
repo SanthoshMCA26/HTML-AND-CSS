e7f4217ae4e953ca8d2d0427de7faa4d9df66d57
santhosh <santhoshrvs26@gmail.com>
2026-08-18 12:06:32 +0530

    Updated login, signup, and dashboard pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
- 
- let btn = document.getElementById('getReadyBtn');
- document.addEventListener('mouseover', (event)=> {
-            btn.textContent = "go";
-        });
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // stop form from refreshing
+
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const message = document.getElementById("message");
+
+      // Example check (replace with your real validation logic)
+      if (email === "admin@gmail.com" && password === "1234") {
+        message.style.color = "green";
+        message.textContent = "Login successful! Redirecting...";
+        setTimeout(() => {
+          window.location.href = "Dashboard.html"; // go to dashboard page
+        }, 1500);
+      } else {
+        message.style.color = "red";
+        message.textContent = "Invalid username or password!";
+        }
+    });
+
+    //for sign submit button
+
+    document.getElementById("registerForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // prevent refresh
+
+      // ✅ After signup, redirect to login page
+      window.location.href = "login.html";
+    });
+
+
